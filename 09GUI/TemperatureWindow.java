668b4302a49379828a92297bd8a562c9c9e7bd5d
robinhanstuy <rhan@stuy.edu>
2017-12-13 14:45:24 -0500

    working on creating radio buttons. bugs not fixed

--- 09GUI/TemperatureWindow.java
+++ 09GUI/TemperatureWindow.java
@@ -1,15 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.events.*;
 public class TemperatureWindow extends JFrame {
  private Container pane;
 
  private JButton b;
- private JLabel l;
+ private JRadioButton f;
  private JTextField t;
- private JCheckBox c;
+ private JRadioButton c;
  
   //CONSTRUCTOR SETS EVERYTHING UP
-  public Window2() {
+  public TemperatureWindow() {
      this.setTitle("Temperature Converter!");
      this.setSize(600,400);
      this.setLocation(100,100);
@@ -19,18 +20,35 @@ public class TemperatureWindow extends JFrame {
      pane.setLayout(new FlowLayout());  //NOTE!! THIS CAN BE CHANGED (see below)
     
      b = new JButton("Convert!");
+     b.addActionListerner(this);
      t = new JTextField(12);
-     c = new JCheckBox("Cel to Far");
-     f = new jCheckBox("Far to cel");
-     pane.add(l);
+     c = new JRadioButton("Cel to Far");
+     c.addActionListerner(this);
+     f = new JRadioButton("Far to cel");
+     c.addActionListerner(this);
+     pane.add(b);
      pane.add(t);
      pane.add(c);
      pane.add(f);
  }
 
+    public void actionPerformed(ActionEvent e){
+	String temp = e.getActionCommand();
+        if (c.isSelected()){
+	    t.setText(cToF(Double.parseDouble(temp)));
+	}
+	
+    }
+    public static double CtoF(double cel){
+	return (9.0/5 * cel) + 32;
+    }
+    public static double FtoC(double far){
+	return (far-32.0) * 5.0 / 9;
+    }
+
   //MAIN JUST INSTANTIATES + MAKE VISIBLE
   public static void main(String[] args) {
-     Window2 g = new Window2();
+     TemperatureWindow g = new TemperatureWindow();
      g.setVisible(true);
   }
 }
